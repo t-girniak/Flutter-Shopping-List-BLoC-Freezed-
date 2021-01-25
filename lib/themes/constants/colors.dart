6f3d3ac378f824f9b9bbcc136507e5453f91da2a
Thomas <33135135+t-girniak@users.noreply.github.com>
2021-01-26 00:36:58 +0200

    Added new color

--- lib/themes/constants/colors.dart
+++ lib/themes/constants/colors.dart
@@ -5,5 +5,6 @@ import 'package:flutter/material.dart';
 class AppColors {
   static final Color black = Colors.black;
   static final Color mustard = Color(0xfffcba00);
+  static final Color lightBlue = Colors.blue.shade200;
   static final Color darkGrey = Color(0xff1d1d1d);
 }
